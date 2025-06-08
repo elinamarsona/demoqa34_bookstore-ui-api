140f89db5962ea71078aa8293757da76f24083ac
Elina Kulikova <elinamarsona@gmail.com>
2025-06-08 22:59:13 +0300

    fix2 TestData and build.gradle

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -27,12 +27,15 @@ public class TestBase {
                 .sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation()); // Игнорируем SSL
 
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "127.0");
-        Configuration.browserSize = System.getProperty("screen_resolution", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
-        Configuration.remote = System.getProperty("remoteUrl",
-                "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remoteUrl");
+        System.out.println(Configuration.browser);
+        System.out.println(Configuration.browserVersion);
+        System.out.println(Configuration.browserSize);
+        System.out.println(Configuration.remote);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
