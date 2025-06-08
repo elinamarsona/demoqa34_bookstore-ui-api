ee1cfb5cc2253375c9efa13a9007ac3e579471c5
Elina Kulikova <elinamarsona@gmail.com>
2025-06-08 22:02:35 +0300

    fix TestData and build.gradle

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -4,11 +4,7 @@ import com.codeborne.selenide.Configuration;
 
 public class TestData {
 
-    public static
-//    String userName = System.getProperty("userName", "test123456"),
-//           password = System.getProperty("password", "Test123456"),
-//           isbn = System.getProperty("isbn", "9781449331818");
-    String userName = "test123456",
-            password = "Test123456@",
-            isbn = "9781449331818";
+    public static final String userName = System.getProperty("userName","elinamarsona@A1");
+    public static final String password = System.getProperty("password","elinamarsona@A1");
+    public static final String isbn = System.getProperty("isbn","9781449331818");
 }
